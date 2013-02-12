c3f7efefdecc37164ec099e8bc810adc17b380a4
Clinton Contreras <ClintonC@zoovy.com>
2013-02-11 16:58:58 -0800

    Category page/homepage/Footer Updates
    - Category page: Built category sidebar link navigation bar. This ONLY
    includes the categories and dropdown menus for the categoies that are on
    the current pages category sidebar. No links have been added at this
    time pending listing and design by Jeff.
    - Homepage: Corrected banner slider having an unnecessary margin on the
    right side/white space and adjust image size to 1000px wide. Current
    whitespace is due to it being present in image. Replacing with actual
    images will remove it.
    - Footer: Updated Read our Blog link with link value. Ready for testing.

--- _chess_init.js
+++ _chess_init.js
@@ -42,17 +42,17 @@ app.rq.push(['templateFunction','homepageTemplate','onDeparts',function(P) {app.
 
 //Header dropdown menus
 var showDropdown = function ($tag) {
-		var $dropdown = $(".dropdown", $tag);
-		var height = 0;
-		$dropdown.children().each(function(){
-			height += $(this).outerHeight(true);
-		});
-		$dropdown.stop().animate({"height":height+"px"}, 1000);
-	}
+	var $dropdown = $(".dropdown", $tag);
+	var height = 0;
+	$dropdown.children().each(function(){
+		height += $(this).outerHeight(true);
+	});
+	$dropdown.stop().animate({"height":height+"px"}, 1000);
+}
 	
-	var hideDropdown = function ($tag) {
-		$(".dropdown", $tag).stop().animate({"height":"0px"}, 1000);
-	}
+var hideDropdown = function ($tag) {
+	$(".dropdown", $tag).stop().animate({"height":"0px"}, 1000);
+}
 
 //Homepage Slideshow image code and carousel code
 app.rq.push(['templateFunction','homepageTemplate','onCompletes',function(P) {
