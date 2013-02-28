2fbc298453e9a8cc4835e339c88a53c7ae8c97f0
Clinton Contreras <ClintonC@zoovy.com>
2013-02-27 17:54:03 -0800

    Homepage/Cart Updates
    - Homepage: updated several category descriptions to fit within their
    allotted spaces. Only the new category of chess potpourri needs a
    category description.
    - Began building of the arrow/indenting feature for the cart where
    component items are indented and have an arrow added to them to indicate
    that they are component parts of an assembly item.

--- extensions/extension_thechessstore.js
+++ extensions/extension_thechessstore.js
@@ -52,6 +52,14 @@ var extension_thechessstore = function() {
 					app.u.dump('BEGIN app.ext.store_category_subcatlist.callbacks.startExtension.onError');
 				}
 			}
+		},
+		renderFormats : {
+			//The render format for determining if a product is a component. Adds a class of assembly to these products that can be used to stylize them.
+			addAssemblyClass : function($tag, data){
+				if(data.value.stid[0] == '%' || data.value.asm_master) {
+					$tag.addClass('assembly');
+				}
+			}
 		}
 	}
 	return r;
