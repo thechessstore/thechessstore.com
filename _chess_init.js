8a2e16785bceccc014c799b9f505cbc3115652cc
clintoncontreras <ClintonC@zoovy.com>
2014-06-23 20:22:26 -0700

    Versioning

--- _chess_init.js
+++ _chess_init.js
@@ -47,7 +47,7 @@ myApp.rq.push(['script',0,myApp.vars.baseURL+'resources/jsonpath.0.8.0.js']); //
 myApp.rq.push(['extension',0,'store_filter','extensions/_thechessstore.js']);
 myApp.rq.push(['extension',0,'extension_thechessstore','extensions/extension_thechessstore.js','startExtension']);
 //myApp.rq.push(['extension',0,'google_remarketing','extensions/google_remarketing.js','startExtension']);
-myApp.rq.push(['extension',0,'google_dynamicremarketing','extensions/partner_google_dynamicremarketing.js','startExtension']);
+//myApp.rq.push(['extension',0,'google_dynamicremarketing','extensions/partner_google_dynamicremarketing.js','startExtension']);
 myApp.rq.push(['extension',0,'prodlist_infinite','extensions/prodlist_infinite.js']);
 myApp.rq.push(['extension',1,'google_adwords','extensions/partner_google_adwords.js','startExtension']);
 myApp.rq.push(['extension',1,'partner_linkconnector','extensions/partner_linkconnector.js','startExtension']);
