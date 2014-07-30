16d2987488aabe2059d50e6a1ce9b242887594d4
clintoncontreras <ClintonC@zoovy.com>
2014-07-30 14:17:26 -0700

    Fix for checkout order overwriting

--- app-quickstart.js
+++ app-quickstart.js
@@ -2283,9 +2283,11 @@ elasticsearch.size = 50;
 				infoObj.trigger = '';
 				infoObj.state = 'init'; //needed for handleTemplateEvents.
 				
+				infoObj.cartid = _app.model.fetchCartID();
+				
 //only create instance once.
 				var $cart = $('#mainContentArea_cart');
-				if($cart.length)	{
+				if($cart.length && $cart.data('cartid') == infoObj.cartid)	{
 					_app.renderFunctions.handleTemplateEvents($cart,infoObj);
 					//the cart has already been rendered.
 					infoObj.trigger = 'refresh';
@@ -2293,7 +2295,6 @@ elasticsearch.size = 50;
 					}
 				else	{
 					infoObj.trigger = 'fetch';
-					infoObj.cartid = _app.model.fetchCartID();
 					$cart = _app.ext.cco.a.getCartAsJqObj(infoObj);
 					_app.renderFunctions.handleTemplateEvents($cart,infoObj);
 					$cart.hide().on('complete',function(){
